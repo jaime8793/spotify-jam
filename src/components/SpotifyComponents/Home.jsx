5113a65ed87e7f56a6202c7fe1fa355fae92a418
jaime <jamesowiti24@gmail.com>
2024-11-19 12:06:56 +0300

    Bugzz

--- src/components/SpotifyComponents/Home.jsx
+++ src/components/SpotifyComponents/Home.jsx
@@ -1,13 +1,44 @@
-export default function Home() {
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function Home() {
+  const [data, setData] = useState(null);
+  // Fetch data from the backend
+  function getSpotifyUserData() {
+    axios
+      .get("http://localhost:3003/api/v1/getUserSpotify")
+      .then((response) => setData(response.data))
+      .catch((error) => console.error("Error fetching Spotify data:", error));
+  }
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3003/api/v1/getUserSpotify`)
+      .then(console.log("Spotify Data Function Working"))
+      .catch((error) => console.error("Error fetching data Home 1:", error));
+  }, []);
   return (
     <>
       <div>This is the home component</div>
-      <a
-        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-        href="http:localhost//3003"
-      >
-        Connect Thy Spotify
-      </a>
+      <div className="flex flex-col">
+        {/* <a
+          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          href="http:localhost//3003"
+          onClick={}
+          //onClick={getSpotifyUserData}
+        >
+          Connect Thy Spotify
+        </a>*/}
+        <a
+          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          //href="http:localhost//3003"
+          onClick={getSpotifyUserData}
+        >
+          Get Thy Data
+        </a>
+        {data ? data : `loading..`}
+      </div>
     </>
   );
 }
+
+export default Home;
